f37427c9f217a0368af457188a76ecada91fb20b
maheshsmp <43444630+maheshsmp@users.noreply.github.com>
2018-11-20 17:02:18 +0530

    Update file1.sh

--- file1.sh
+++ file1.sh
@@ -1,6 +1,6 @@
 # /bin/bash/
-hi my name is mahesh
+echo  "hi my name is mahesh
 so this is the good working 
 hyderabad is very polluted
 banglore is more densitive state
-to compare than the m.p
+to compare than the m.p"
